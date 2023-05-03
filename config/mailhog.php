829b8a2cc0dca645efbd712dd647ea91a9682ebd
alisaleem27 <alisaleem27@users.noreply.github.com>
2023-05-03 13:46:44 +0000

    Fix styling

--- config/mailhog.php
+++ config/mailhog.php
@@ -2,12 +2,12 @@
 
 // config for AliSaleem/Mailhog
 return [
-    'transport'    => 'smtp',
-    'host'         => env('MAIL_HOST', '127.0.0.1'),
-    'port'         => env('MAIL_PORT', 1025),
-    'encryption'   => env('MAIL_ENCRYPTION', 'null'),
-    'username'     => env('MAIL_USERNAME'),
-    'password'     => env('MAIL_PASSWORD'),
-    'timeout'      => null,
+    'transport' => 'smtp',
+    'host' => env('MAIL_HOST', '127.0.0.1'),
+    'port' => env('MAIL_PORT', 1025),
+    'encryption' => env('MAIL_ENCRYPTION', 'null'),
+    'username' => env('MAIL_USERNAME'),
+    'password' => env('MAIL_PASSWORD'),
+    'timeout' => null,
     'local_domain' => env('MAIL_EHLO_DOMAIN'),
 ];
